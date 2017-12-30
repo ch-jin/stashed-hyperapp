2e7538a877649605149057ab895f5b755217bb76
Jin Chen <jinchen93@gmail.com>
2017-12-30 00:50:10 -0800

    Add GET filenames API endpoint

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const app = express();
 
 app.use(express.static(__dirname));
 
-app.listen(8080, () => console.log('Stashed is listening on 8080'));
+app.get('/api/files', (req, res) => {
+  const pathToFiles = path.join(
+    __dirname,
+    process.env['STASHED_DIR_PATH']
+  );
+  fs.readdir(pathToFiles, (err, fileNames) => {
+    // Exclude dot files
+    const data = fileNames.filter(fileName => fileName[0] !== '.');
+    return res.json(data);
+  });
+});
+
+app.listen(8080, () => console.log('stashed-hyperapp is listening on 8080'));
