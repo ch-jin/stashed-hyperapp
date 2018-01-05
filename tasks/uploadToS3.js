9150c2c35875f3e5936b0606b890caa440f3b195
Jin Chen <jinchen93@gmail.com>
2018-01-05 01:28:35 -0800

    Add AWS S3 / DO Space file uploader

---
+++ tasks/uploadToS3.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+const AWS = require('aws-sdk');
+const s3Stream = require('s3-upload-stream');
+const settings = require('../settings.json');
+
+const { endpoint, bucket, directories } = settings.s3;
+
+AWS.config.setPromisesDependency(require('bluebird'));
+AWS.config.update({
+  endpoint: new AWS.Endpoint(endpoint),
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_SECRET_KEY,
+});
+
+const s3Stream = s3UploadStream(new AWS.S3());
+
+new AWS.S3()
+  .listObjects({ Bucket: bucket })
+  .promise()
+  .then(data => {
+    const bucketFiles = data.Contents.map(obj => obj.Key);
+    directories.forEach(dir => {
+      fs.readdir(path.join(__dirname, dir.path), (err, files) => {
+        files.forEach(fileName => {
+          const fileDest = dir.bucketFolder + fileName;
+          if (bucketFiles.includes(fileDest)) {
+            return;
+          }
+
+          console.log(`Uploading ${fileName}`);
+
+          const splitByPeriods = fileName.split('.');
+          if (splitByPeriods.length === 1) {
+            return;
+          }
+
+          const readStream = fs.createReadStream(
+            path.join(__dirname, dir.path, fileName)
+          );
+
+          const s3Upload = s3Stream.upload({
+            Bucket: bucket,
+            Key: fileDest,
+            ACL: 'public-read',
+          });
+
+          s3Upload.maxPartSize(20971520);
+
+          readStream
+            .pipe(s3Upload)
+            .on('finish', () =>
+              console.log(`${fileName} successfully uploaded.\n\n`)
+            );
+        });
+      });
+    });
+  });
