88f0583c8bfa6d58dc34a5e0aa03293ed491097f
Jin Chen <jinchen93@gmail.com>
2018-01-05 22:21:26 -0800

    Extrapolate S3 listing, uploading, and credential setting into different files

--- tasks/uploadToS3.js
+++ tasks/uploadToS3.js
@@ -3,56 +3,52 @@ const path = require('path');
 const AWS = require('aws-sdk');
 const s3UploadStream = require('s3-upload-stream');
 const SETTINGS = require('../settings.json');
+const listS3Files = require('../lib/listS3Files');
+const setAWSCredentials = require('../lib/setAWSCredentials');
 
-const { endpoint, bucket, directories } = SETTINGS.s3;
+const { directories, bucket } = SETTINGS.S3;
 
-AWS.config.setPromisesDependency(require('bluebird'));
-AWS.config.update({
-  endpoint: new AWS.Endpoint(endpoint),
-  accessKeyId: process.env.S3_ACCESS_KEY,
-  secretAccessKey: process.env.S3_SECRET_KEY,
-});
-
+setAWSCredentials(AWS);
 const s3Stream = s3UploadStream(new AWS.S3());
 
-new AWS.S3()
-  .listObjects({ Bucket: bucket })
-  .promise()
-  .then(data => {
-    const bucketFiles = data.Contents.map(obj => obj.Key);
-    directories.forEach(dir => {
-      fs.readdir(path.join(__dirname, dir.path), (err, files) => {
-        files.forEach(fileName => {
-          const fileDest = dir.bucketFolder + fileName;
-          if (bucketFiles.includes(fileDest)) {
-            return;
-          }
+const uploadToS3 = bucketData => {
+  const bucketFiles = bucketData.Contents.map(obj => obj.Key);
 
-          console.log(`Uploading ${fileName}`);
+  directories.forEach(dir => {
+    fs.readdir(path.join(__dirname, dir.path), (err, files) => {
+      files.forEach(fileName => {
+        const fileDest = dir.bucketFolder + fileName;
+        if (bucketFiles.includes(fileDest)) {
+          return;
+        }
 
-          const splitByPeriods = fileName.split('.');
-          if (splitByPeriods.length === 1) {
-            return;
-          }
+        console.log(`Uploading ${fileName}`);
 
-          const readStream = fs.createReadStream(
-            path.join(__dirname, dir.path, fileName)
-          );
+        const splitByPeriods = fileName.split('.');
+        if (splitByPeriods.length === 1) {
+          return;
+        }
 
-          const s3Upload = s3Stream.upload({
-            Bucket: bucket,
-            Key: fileDest,
-            ACL: 'public-read',
-          });
+        const readStream = fs.createReadStream(
+          path.join(__dirname, dir.path, fileName)
+        );
 
-          s3Upload.maxPartSize(20971520);
-
-          readStream
-            .pipe(s3Upload)
-            .on('finish', () =>
-              console.log(`${fileName} successfully uploaded.\n\n`)
-            );
+        const s3Upload = s3Stream.upload({
+          Bucket: bucket,
+          Key: fileDest,
+          ACL: 'public-read',
         });
+
+        s3Upload.maxPartSize(20971520);
+
+        readStream
+          .pipe(s3Upload)
+          .on('finish', () =>
+            console.log(`${fileName} successfully uploaded.\n\n`)
+          );
       });
     });
   });
+};
+
+listS3Files(uploadToS3);
