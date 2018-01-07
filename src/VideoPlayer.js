8d5e6978fcbe456acfd464de963557c743186d25
Jin Chen <jinchen93@gmail.com>
2018-01-06 21:20:52 -0800

    Implement basic video control bar

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,19 +1,48 @@
 import { h } from 'hyperapp';
+import classNames from 'classnames';
 
-export const VideoPlayer = ({ selectedFile }) =>
-  selectedFile.type === 'video' ? (
-    <video key={selectedFile.name} controls autoplay crossorigin="anonymous">
-      <source src={selectedFile.path} />
-      {selectedFile.subtitleSrc && (
-        <track
-          default
-          label="English"
-          kind="subtitles"
-          srclang="en"
-          src={selectedFile.subtitleSrc}
-        />
-      )}
-    </video>
-  ) : null;
+export const VideoPlayer = ({
+  toggleVideoPlayerHover,
+  selectedFile,
+  videoPlayer: { isHover },
+}) => {
+  if (selectedFile.type !== 'video') {
+    return null;
+  }
+
+  const videoContainerClass = classNames({
+    'video-container': true,
+    'video-controls-hide': !isHover,
+  });
+
+  return (
+    <div
+      onmouseenter={toggleVideoPlayerHover}
+      onmouseleave={toggleVideoPlayerHover}
+      className={videoContainerClass}
+    >
+      <video key={selectedFile.name} autoplay crossorigin="anonymous">
+        <source src={selectedFile.path} />
+        {selectedFile.subtitleSrc && (
+          <track
+            default
+            label="English"
+            kind="subtitles"
+            srclang="en"
+            src={selectedFile.subtitleSrc}
+          />
+        )}
+      </video>
+      <div className="video-controls">
+        <div className="left-controls">
+          <i className="play-pause fa fa-play" />
+        </div>
+        <div className="right-controls">
+          <i className="play-pause fa fa-expand" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default VideoPlayer;
--- src/index.js
+++ src/index.js
@@ -9,23 +9,40 @@ const appState = {
     path: '',
     type: '',
   },
+  videoPlayer: {
+    isHover: false,
+  },
 };
 
 const appActions = {
   fetchFile: () => state => fetch('/api/files').then(res => res.json()),
   replaceFile: files => ({ files }),
   selectFile: file => ({ selectedFile: file }),
+  toggleVideoPlayerHover: () => ({ videoPlayer }) => ({
+    videoPlayer: Object.assign({}, videoPlayer, {
+      isHover: !videoPlayer.isHover,
+    }),
+  }),
 };
 
 const view = (state, actions) => {
-  const { files, selectedFile } = state;
-  const { fetchFile, replaceFile, selectFile } = actions;
+  const { files, selectedFile, videoPlayer } = state;
+  const {
+    fetchFile,
+    replaceFile,
+    selectFile,
+    toggleVideoPlayerHover,
+  } = actions;
 
   const fetchAndReceiveFile = () => fetchFile().then(data => replaceFile(data));
 
   return (
     <div oncreate={fetchAndReceiveFile}>
-      <VideoPlayer selectedFile={selectedFile} />
+      <VideoPlayer
+        toggleVideoPlayerHover={toggleVideoPlayerHover}
+        videoPlayer={videoPlayer}
+        selectedFile={selectedFile}
+      />
       <div className="menu-container">
         <h1>Stashed</h1>
         <FileList files={files} selectFile={selectFile} />
